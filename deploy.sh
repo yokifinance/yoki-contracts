10384177db39a4b8719a42f61ac095d1f85a1fec
SergeyOkishev <sokishev@usetech.com>
2023-07-07 15:37:14 +0300

    Fixed deploy container.

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,11 @@ echo Waiting for testnet to startup...
 # wait for testnet to start fully
 sleep 1s
 
-RPC=http://0.0.0.0:8545
-echo USDT balance of {$address} is
-cast call 0xc2132d05d31c914a87c6611c10748aeb04b58e8f "balanceOf(address)(uint256)" $address
+RPC=http://yoki-testnet:8545
+cd app
+
+echo USDT balance of $address
+cast call -r $RPC 0xc2132d05d31c914a87c6611c10748aeb04b58e8f "balanceOf(address)(uint256)" $address
 echo Deploying contracts
 echo DCAV3Factory
-forge create DCAV3Factory --constructor-args 0x59698022f08FF0d0B6EDbC956Fd9c0596543A983 0xf52Aea45dFDE4669C73010D4C47E9e0c75E5c8ca --mnemonic-passphrase "$mnemonic" --rpc-url $infura_url --legacy
+forge create -r $RPC DCAV3Factory --constructor-args 0x59698022f08FF0d0B6EDbC956Fd9c0596543A983 0xf52Aea45dFDE4669C73010D4C47E9e0c75E5c8ca --unlocked --from $address --legacy
