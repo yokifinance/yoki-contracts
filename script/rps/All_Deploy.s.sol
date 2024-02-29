8a74f531c6af94fde9924427cdba7f0b8f18cfe5
SergeyOkishev <sokishev@usetech.com>
2024-03-01 01:45:46 +0300

    Cleariy env example. Updated admins list for deploy all.

--- script/rps/All_Deploy.s.sol
+++ script/rps/All_Deploy.s.sol
@@ -14,8 +14,10 @@ contract DeployAll is Script {
         console.log("Deploying RPSV1 implementation for cloning");
         RPSV1 rpsImp = new RPSV1();
         console.log("RPSV1 deployed: ", address(rpsImp));
-        address[] memory admins = new address[](1);
-        admins[0] = vm.envAddress("address");
+        address[] memory admins = new address[](3);
+        admins[0] = address(0x94Ad54EC1299B9BE82eCc9328187eF37fDB07329);
+        admins[1] = address(0x2D9a8BE931f1EAb82ABFCb9697023424E440CD43);
+        admins[2] = address(0xB0b12f40b18027f1a2074D2Ab11C6e0d6c6acbB5);
 
         console.log("Deploying RPSV1Factory");
         RPSV1Factory factory = new RPSV1Factory(address(rpsImp), admins);
